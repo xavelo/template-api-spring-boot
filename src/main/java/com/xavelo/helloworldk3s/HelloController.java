2066326b9b04ad0cd541b5b14c79f0f1a406d67f
xavelo <xavelo@gmail.com>
2024-05-24 21:51:01 +0200

    commit id

--- src/main/java/com/xavelo/helloworldk3s/HelloController.java
+++ src/main/java/com/xavelo/helloworldk3s/HelloController.java
@@ -3,9 +3,14 @@ package com.xavelo.helloworldk3s;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    private Environment env;
 
     @Value("${HOSTNAME:unknown}")
     private String podName;
@@ -14,9 +19,10 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
+        String commitId = env.getProperty("git.commit.id");
         requests += 1;
         System.out.println("/hello from " + podName + " - " + requests);
-        return "Hello world ArgoCD from " + podName;
+        return "Hello world ArgoCD from " + podName + " - commitId " + commitId;
     }
 
     @GetMapping("/test")
