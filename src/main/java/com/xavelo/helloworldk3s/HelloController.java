2e06aea1279a0b690b0c206de1538a3a9851c04b
xavelo <xavelo@gmail.com>
2024-05-25 22:56:18 +0200

    commitID + branch

--- src/main/java/com/xavelo/helloworldk3s/HelloController.java
+++ src/main/java/com/xavelo/helloworldk3s/HelloController.java
@@ -1,17 +1,12 @@
 package com.xavelo.helloworldk3s;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.info.GitProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 
 @RestController
 public class HelloController {
 
-    @Autowired
-    private GitProperties gitProperties;
 
     @Value("${HOSTNAME:unknown}")
     private String podName;
@@ -20,8 +15,8 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        String commitId = gitProperties.getShortCommitId();
-        String branch = gitProperties.getBranch();
+        String commitId = "ccc";
+        String branch = "master";
 
         requests += 1;
         System.out.println("/test from " + podName + " - " + commitId + " - " + branch);
