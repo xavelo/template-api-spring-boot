39d1454095fd20d1036db09661c4003665930a6a
xavelo <xavelo@gmail.com>
2024-05-26 17:27:03 +0200

    commit ID

--- src/main/java/com/xavelo/helloworldk3s/HelloController.java
+++ src/main/java/com/xavelo/helloworldk3s/HelloController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-
     @Value("${HOSTNAME:unknown}")
     private String podName;
+
+    @Value("${git.commit.id}")
+    private String gitCommitId;
 
     int requests = 0;
 
@@ -19,8 +21,8 @@ public class HelloController {
         String branch = "master";
 
         requests += 1;
-        System.out.println("/test 0.1.0 from " + podName + " - " + commitId + " - " + branch);
-        return "0.1.0 test world ArgoCD from " + podName + " ==> commitID " + commitId + " - branch " + branch;
+        System.out.println("/test 0.1.0 from " + podName + " - " + commitId);
+        return "version " + commitId + " - pod " + podName + " ==> commitID " + commitId;
     }
 
 }
--- src/main/java/com/xavelo/helloworldk3s/HelloWorldK3sApplication.java
+++ src/main/java/com/xavelo/helloworldk3s/HelloWorldK3sApplication.java
@@ -2,8 +2,10 @@ package com.xavelo.helloworldk3s;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:git.properties")
 public class HelloWorldK3sApplication {
 
 	public static void main(String[] args) {
