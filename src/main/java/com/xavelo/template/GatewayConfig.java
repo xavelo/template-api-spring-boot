f2d918738c0604972a6ab55f29ac3b3028b1d1b2
xavelo <xavelo@gmail.com>
2024-09-07 16:31:25 +0200

    RequestRateLimiter

---
+++ src/main/java/com/xavelo/template/GatewayConfig.java
@@ -0,0 +1,18 @@
+package com.xavelo.template;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    KeyResolver ipKeyResolver() {
+        return exchange -> {
+            String ip = exchange.getRequest().getRemoteAddress().getAddress().getHostAddress();
+            return Mono.just(ip);
+        };
+    }
+}
--- src/main/java/com/xavelo/template/TemplateApplication.java
+++ src/main/java/com/xavelo/template/TemplateApplication.java
@@ -2,9 +2,11 @@ package com.xavelo.template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @PropertySource("classpath:git.properties")
 public class TemplateApplication {
 
