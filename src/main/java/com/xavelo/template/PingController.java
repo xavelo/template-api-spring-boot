40c80376cdfa84da7ef9b1f8a4f994efb6a21c25
xavelo <xavelo@gmail.com>
2024-09-07 16:44:40 +0200

    reactive

--- src/main/java/com/xavelo/template/PingController.java
+++ src/main/java/com/xavelo/template/PingController.java
@@ -1,17 +1,17 @@
 package com.xavelo.template;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.info.GitProperties;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 public class PingController {
@@ -25,13 +25,13 @@ public class PingController {
     private GitProperties gitProperties;
 
     @GetMapping("/ping")
-    public ResponseEntity<String> ping() {
+    public Mono<String> ping() {
         String commitId = gitProperties.getCommitId();
         LocalDateTime dateTime = LocalDateTime.ofInstant(gitProperties.getCommitTime(), ZoneId.systemDefault());
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String commitTime = dateTime.format(formatter);
         logger.info("pong from pod {} - commitId {} - commitTime {}", commitId, commitTime, podName);
-        return ResponseEntity.ok("pong from pod " + podName + " - " + commitId + " - " + commitTime);
+        return Mono.just("pong from pod " + podName + " - " + commitId + " - " + commitTime);
     }
 
 }
