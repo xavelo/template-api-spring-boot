c5c670fcf72db2029791935e7b99f5da3d9ce335
xavelo <xavelo@gmail.com>
2024-10-27 17:58:39 +0100

    first message

---
+++ src/main/java/com/xavelo/template/ApiSongQuotesApplication.java
@@ -0,0 +1,13 @@
+package com.xavelo.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSongQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSongQuotesApplication.class, args);
+	}
+
+}
--- src/test/java/com/xavelo/template/ApiSongQuotesApplicationTests.java
+++ src/test/java/com/xavelo/template/ApiSongQuotesApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
-class TemplateApplicationTests {
+class ApiApiSongQuotesApplicationTests {
 
 	@Autowired
 	private ApplicationContext applicationContext;
