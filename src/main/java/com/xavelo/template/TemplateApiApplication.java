01dcd46e4342030188af74c22a7a40d3067e5aa5
xavelo <xavelo@gmail.com>
2025-04-16 22:47:14 +0200

    cleanup

--- src/main/java/com/xavelo/template/TemplateApiApplication.java
+++ src/main/java/com/xavelo/template/TemplateApiApplication.java
@@ -2,12 +2,8 @@ package com.xavelo.template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@PropertySource("classpath:git.properties")
 public class TemplateApiApplication {
 
 	public static void main(String[] args) {
