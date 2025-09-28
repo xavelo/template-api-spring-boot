0fc735893bf665e3502871499bd112283aef5329
xavelo <xavelo@gmail.com>
2025-09-28 14:37:09 +0200

    Split project into API and application modules

--- application/src/main/java/com/xavelo/template/api/adapter/in/http/secure/SecurePingController.java
+++ application/src/main/java/com/xavelo/template/api/adapter/in/http/secure/SecurePingController.java
@@ -1,26 +1,26 @@
 package com.xavelo.template.api.adapter.in.http.secure;
 
+import com.xavelo.api.dto.PingResponse;
+import com.xavelo.api.interfaces.SecurePingApi;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/secure")
-public class SecurePingController {
+public class SecurePingController implements SecurePingApi {
 
     private static final Logger logger = LogManager.getLogger(SecurePingController.class);
 
     @Value("${HOSTNAME:unknown}")
     private String podName;
 
-    @GetMapping("/ping")
-    public ResponseEntity<String> ping() {
-        return ResponseEntity.ok("pong from " + podName);
+    @Override
+    public ResponseEntity<PingResponse> getSecurePing() {
+        String message = "pong from " + podName;
+        logger.info("Responding to /api/secure/ping with message: {}", message);
+        PingResponse response = new PingResponse().message(message);
+        return ResponseEntity.ok(response);
     }
-
 }
-
