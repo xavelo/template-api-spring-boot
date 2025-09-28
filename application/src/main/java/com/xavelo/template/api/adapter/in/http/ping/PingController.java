f864d99488a45607af135fcd116f5696c47d58a3
xavelo <xavelo@gmail.com>
2025-09-28 15:31:25 +0200

    Refactor controllers to use generated OpenAPI models

--- application/src/main/java/com/xavelo/template/api/adapter/in/http/ping/PingController.java
+++ application/src/main/java/com/xavelo/template/api/adapter/in/http/ping/PingController.java
@@ -1,26 +1,29 @@
 package com.xavelo.template.api.adapter.in.http.ping;
 
-import com.xavelo.api.dto.PingResponse;
-import com.xavelo.api.interfaces.PingApi;
+import com.xavelo.template.api.api.model.PingResponseDto;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PingController implements PingApi {
+@RequestMapping(path = "/api")
+public class PingController {
 
     private static final Logger logger = LogManager.getLogger(PingController.class);
 
     @Value("${HOSTNAME:unknown}")
     private String podName;
 
-    @Override
-    public ResponseEntity<PingResponse> getPing() {
+    @GetMapping(path = "/ping", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PingResponseDto> getPing() {
         String message = "pong from " + podName;
         logger.info("Responding to /api/ping with message: {}", message);
-        PingResponse response = new PingResponse().message(message);
+        PingResponseDto response = new PingResponseDto().message(message);
         return ResponseEntity.ok(response);
     }
 }
