e7383207da8edadabe88fd7d4fa2d3e49bc9c7b8
xavelo <xavelo@gmail.com>
2025-10-05 20:49:29 +0200

    Switch Kafka listener to ConsumerRecord signature

--- application/src/main/java/com/xavelo/template/adapter/in/kafka/EventConsumer.java
+++ application/src/main/java/com/xavelo/template/adapter/in/kafka/EventConsumer.java
@@ -8,7 +8,9 @@ import com.xavelo.template.application.domain.Event;
 import com.xavelo.template.application.port.in.ProcessEventUseCase;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
 import static com.xavelo.common.metrics.AdapterMetrics.Direction.IN;
@@ -33,10 +35,13 @@ public class EventConsumer {
         containerFactory = "testTopicEventKafkaListenerContainerFactory"
     )
     @CountAdapterInvocation(name = "item-events", direction = IN, type = KAFKA)
-    public void consume(String payload) {
+    public void consume(ConsumerRecord<String, String> record, Acknowledgment acknowledgment) {
         try {
-            Event event = objectMapper.readValue(payload, Event.class);
+            Event event = objectMapper.readValue(record.value(), Event.class);
             processEventUseCase.process(event);
+            if (acknowledgment != null) {
+                acknowledgment.acknowledge();
+            }
         } catch (JsonProcessingException e) {
             logger.error("Unable to deserialize event payload", e);
             throw new IllegalStateException("Failed to deserialize event", e);
