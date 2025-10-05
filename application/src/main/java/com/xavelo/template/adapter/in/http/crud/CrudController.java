d859decc8990b00837990927a655111b894ef531
xavelo <xavelo@gmail.com>
2025-10-05 11:01:19 +0200

    refactor

--- application/src/main/java/com/xavelo/template/adapter/in/http/crud/CrudController.java
+++ application/src/main/java/com/xavelo/template/adapter/in/http/crud/CrudController.java
@@ -10,6 +10,7 @@ import com.xavelo.template.api.contract.model.CrudObjectPageDto;
 import com.xavelo.template.application.port.in.CrudUseCase;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -28,32 +29,34 @@ public class CrudController implements CrudApi {
         this.crudUseCase = crudUseCase;
     }
 
-    @CountAdapterInvocation(name = "crud-list", direction = AdapterMetrics.Direction.IN, type = AdapterMetrics.Type.HTTP)
     @Override
-    public ResponseEntity<CrudObjectPageDto> listCrudObjects(Integer page, Integer size, String sort) {
-        CrudObjectPageDto response = crudUseCase.listCrudObjects(page, size, sort);
-        logger.info("Listing CrudObjects page={}, size={}, sort={}", page, size, sort);
-        return ResponseEntity.ok(response);
+    @CountAdapterInvocation(name = "crud-create", direction = AdapterMetrics.Direction.IN, type = AdapterMetrics.Type.HTTP)
+    public ResponseEntity<Void> createCrudObject(CrudObjectCreateRequestDto crudObjectCreateRequestDto) {
+        CrudObjectDto created = crudUseCase.createCrudObject(crudObjectCreateRequestDto);
+        logger.info("Created CrudObject {}", created.getId());
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{crudObjectId}")
+                .buildAndExpand(created.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 
-    @CountAdapterInvocation(name = "crud-get", direction = AdapterMetrics.Direction.IN, type = AdapterMetrics.Type.HTTP)
     @Override
+    @CountAdapterInvocation(name = "crud-list", direction = AdapterMetrics.Direction.IN, type = AdapterMetrics.Type.HTTP)
+    public ResponseEntity<CrudObjectPageDto> listCrudObjects(Integer page, Integer size, String sort) {
+        CrudObjectPageDto list = crudUseCase.listCrudObjects(page, size, sort);
+        logger.info("Listing CrudObjects page={}, size={}, sort={}", page, size, sort);
+        return ResponseEntity.ok(list);
+    }
+
+    @Override
+    @CountAdapterInvocation(name = "crud-get", direction = AdapterMetrics.Direction.IN, type = AdapterMetrics.Type.HTTP)
     public ResponseEntity<CrudObjectDto> getCrudObject(String crudObjectId) {
         CrudObjectDto crudObject = crudUseCase.getCrudObject(crudObjectId);
         logger.info("Retrieved CrudObject {}", crudObjectId);
         return ResponseEntity.ok(crudObject);
     }
 
-    @CountAdapterInvocation(name = "crud-create", direction = AdapterMetrics.Direction.IN, type = AdapterMetrics.Type.HTTP)
-    @Override
-    public ResponseEntity<Void> createCrudObject(CrudObjectCreateRequestDto crudObjectCreateRequestDto) {
-        CrudObjectDto created = crudUseCase.createCrudObject(crudObjectCreateRequestDto);
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-            .path("/{crudObjectId}")
-            .buildAndExpand(created.getId())
-            .toUri();
-        logger.info("Created CrudObject {}", created.getId());
-        return ResponseEntity.created(location).build();
-    }
+
 
 }
