667cfae3c3724ec1de0a5c39c4888804d614a297
xavelo <xavelo@gmail.com>
2025-10-05 12:20:27 +0200

    Annotate joke adapters for metrics

--- application/src/main/java/com/xavelo/template/adapter/out/http/joke/ChuckNorrisJokeAdapter.java
+++ application/src/main/java/com/xavelo/template/adapter/out/http/joke/ChuckNorrisJokeAdapter.java
@@ -1,15 +1,17 @@
 package com.xavelo.template.adapter.out.http.joke;
 
+import com.xavelo.common.metrics.Adapter;
+import com.xavelo.common.metrics.AdapterMetrics;
+import com.xavelo.common.metrics.CountAdapterInvocation;
 import com.xavelo.template.application.domain.joke.Joke;
 import com.xavelo.template.application.port.out.joke.GetRandomJokePort;
 import com.xavelo.template.configuration.ChuckNorrisProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Adapter
 public class ChuckNorrisJokeAdapter implements GetRandomJokePort {
 
     private final RestTemplate restTemplate;
@@ -21,6 +23,10 @@ public class ChuckNorrisJokeAdapter implements GetRandomJokePort {
     }
 
     @Override
+    @CountAdapterInvocation(
+            name = "chuck-norris-random",
+            direction = AdapterMetrics.Direction.OUT,
+            type = AdapterMetrics.Type.HTTP)
     public Joke getRandomJoke() {
         try {
             ResponseEntity<ChuckNorrisJokeResponse> response = restTemplate.getForEntity("/jokes/random", ChuckNorrisJokeResponse.class);
